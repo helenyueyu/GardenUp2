88d48dc2af16d495ebbfd201eb1a684b7916da11
tokyosuite <helenyueyu@gmail.com>
2018-10-27 13:29:30 -0700

    Changed the main screen two labels ("Vegetables", "Fruits") to buttons. Added new vegetable images to assets.
    Added a bunch of view controllers, also added back button.

---
+++ GardenUp/VegetableDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VegetableDetailViewController.swift
+//  GardenUp
+//
+//  Created by Helen Yu on 10/27/18.
+//  Copyright © 2018 Helen Yu. All rights reserved.
+//
+
+import UIKit
+
+class VegetableDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
